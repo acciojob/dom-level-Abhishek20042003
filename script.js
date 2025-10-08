3092315d3270d0933bf816de0a98a80b952910a3
Abhishek20042003 <141939848+Abhishek20042003@users.noreply.github.com>
2025-10-08 13:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let element = document.querySelector('level')
+let currentelement = element;
+let count = 0;
+
+while(currentelement != undefined;){
+count++;
+currentelement= currentelement.parentElement;
+	
+	}
+Alert('The level of the element is: $(count)')
